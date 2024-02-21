169234a8b8ffe67fc7777767e3921a4e893a5b77
ZhMax <zhelninmax@gmail.com>
2024-02-21 11:37:37 +0300

    remove some files

--- llm-tune/configs/llama_instruct_3bit_orig.py
+++ llm-tune/configs/llama_instruct_3bit_orig.py
@@ -14,40 +14,40 @@ def model_configs():
     data.dataset_config_name = None
     data.source_max_len = 16
     data.target_max_len = 512
-    data.dataset_percentage = 10
+    data.dataset_percentage = 100
     data.validation_split_percentage = 5
-    data.max_seq_length = 512
+    data.max_seq_length = 2048
     data.trust_remote_code = True
     data.preprocessing_num_workers = 8
 
 
     ### MODEL CHECKPOINT ###
     config.model_type = 'Auto'
-    config.model_name_or_path = '/home/projects/LLaMA/huggingface/Llama-2-7b-hf'
+    config.model_name_or_path = '/home/llm_compression/Quantization/Quik/weights/llama7b_3w_16a_128fp'
     config.model_config_name = None
     config.tokenizer_name = None
     config.token = None
     config.use_fast_tokenizer = True
     config.trust_remote_code = True
-    config.max_memory = 21
+    config.max_memory = 79
 
     ### SAVING DIRS ###
     # config.cache_dir = None
-    config.output_dir = './exp_results/output/llama7b_test'
+    config.output_dir = './exp_results/instruct/quik3bit_flan'
 
     ### TRAINING ###
     config.run_name = 'linearquant_3bit'
     config.resume_from_checkpoint = None
     # config.num_train_epochs = None
     config.max_steps = 4000
-    config.learning_rate = 5e-5
-    config.weight_decay = 0.0
-    config.lr_scheduler_type = 'constant'
+    config.learning_rate = 1e-4
+    config.weight_decay = 0.1
+    config.lr_scheduler_type = 'linear'
     config.warmup_ratio =  0.03
     config.seed = 11
-    config.per_device_train_batch_size = 1
-    config.per_device_eval_batch_size = 1
-    config.gradient_accumulation_steps = 64
+    config.per_device_train_batch_size = 4
+    config.per_device_eval_batch_size = 4
+    config.gradient_accumulation_steps = 8
     config.gradient_checkpointing = False
     config.report_to = None
     ### eval ###
@@ -76,8 +76,7 @@ def model_configs():
     ### LinearQuantNoise
     config.LinearQuantNoise = {
         'replace_Linear': True,
-        # 'path_to_act_scales': "/home/LLM_Compression/QUIK/experiments/act_scales/Llama-2-7b-hf.pt",
-        'path_to_act_scales': "/home/projects/Quantization/QUIK/experiments/act_scales/Llama-2-7b-hf.pt",
+        'path_to_act_scales': "/home/LLM_Compression/QUIK/experiments/act_scales/Llama-2-7b-hf.pt",
         'fp_features_num': 128,
         'quant_target_modules': ["q_proj", "k_proj", "v_proj", "o_proj", "up_proj", "down_proj", "gate_proj"],
         'quant_bit': 3,
