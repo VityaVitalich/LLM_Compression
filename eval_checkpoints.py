13f34e161d0b013e341bd56b393ebd48f0171089
VityaVitalich <vvmoskvoretskiy@yandex.ru>
2024-03-18 14:07:12 +0300

    add paper evaluation and parallel

--- eval_checkpoints.py
+++ eval_checkpoints.py
@@ -11,11 +11,19 @@ if __name__ == '__main__':
     
     parser.add_argument("--model_dir", help="path to model", required=True, type=str)
     parser.add_argument("--save_dir", help="path to saving results", required=True, type=str)
+    parser.add_argument("--task", default="paper", type=str) # paper of huawei
+    parser.add_argument("--bs", default=16, type=int, help="batch size")
     args = parser.parse_args()
     parent_directory = args.model_dir
     save_path = args.save_dir
     prefix = 'checkpoint-'
 
+    if args.task == 'paper':
+        tasks = 'hellaswag,boolq,winogrande,piqa,ai2_arc'
+    elif args.task == 'huawei':
+        tasks = 'hellaswag,boolq,swag,winogrande,xwinograd_en'
+    else:
+        raise AttributeError(f"No such task {args.task}")
     # Iterate over all items in the parent directory
     for item in os.listdir(parent_directory):
         # Construct the full path of the item
@@ -30,7 +38,7 @@ if __name__ == '__main__':
 
         
              result_path = f'{save_path}_ckpt{checkpoint_num}/'
-             command = f'lm_eval --model hf     --model_args pretrained={item_path} \
-        --tasks hellaswag,swag,winogrande,boolq,xwinograd_en    --device cuda:0     --batch_size 16  --output_path {result_path}'
+             command = f'lm_eval --model hf     --model_args pretrained={item_path},parallelize=True \
+        --tasks {tasks}     --batch_size {args.bs}  --output_path {result_path}'
              os.system(command)
 
