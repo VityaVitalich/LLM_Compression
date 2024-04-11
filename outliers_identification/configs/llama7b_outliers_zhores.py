707320b532c294dcd355adac6dc3c07accc62dd2
ZhMax <zhelninmax@gmail.com>
2024-04-11 10:36:26 +0000

    outliers identification

--- outliers_identification/configs/llama7b_outliers_zhores.py
+++ outliers_identification/configs/llama7b_outliers_zhores.py
@@ -7,7 +7,7 @@ def model_configs():
     ### DATASET ###
     data = config.data = ml_collections.ConfigDict()
     data.dataset_path = "/home/llm_compression/Quantization/Weight_scales/datasets/val.jsonl.zst"
-    data.output_path = "/home/llm_compression/Quantization/Weight_scales/obs_scales/llama7b_wanda_scales_3bit_wiki.pt"
+    data.output_path = "/home/LLaMA/huggingface/act_scales/tulu-2-7b_activation_estimator.pt"
     data.max_seq_length = 512
     data.num_samples = 512
     data.trust_remote_code = True
@@ -15,17 +15,17 @@ def model_configs():
 
     ### MODEL CHECKPOINT ###
     config.model_type = 'Auto'
-    config.model_name_or_path = '/home/llm_compression/LLaMA/Llama-2-7b-hf'
+    config.model_name_or_path = '/home/LLaMA/huggingface/tulu-2-7b'
     config.token = None
     config.use_fast_tokenizer = True
     config.trust_remote_code = True
 
     ### Estimator ###
     config.estimator = {
-        'estimator':'Wanda_Estimator', #'OBS_Estimator',
+        'estimator':'Activation_Estimator', #'OBS_Estimator',
         'agg': 'max',
         'add_quantizer': True,
-        'bit': 3
+        'bit': 0
     }
 
     return config
