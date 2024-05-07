647328fe1a78deb348d7efb7dae698d404e54540
VityaVitalich <vvmoskvoretskiy@yandex.ru>
2024-05-07 15:43:21 +0300

    parameters from paper

--- AQLM/quantize.sh
+++ AQLM/quantize.sh
@@ -1,24 +1,29 @@
-export CUDA_VISIBLE_DEVICES=0   # or e.g. 0,1,2,3
-export MODEL_PATH='/home/data/compression/Llama-2-7b-hf'
-export DATASET_PATH='wikitext2' #'/home/data/compression/aqlm_cache/red_pajama_llama.pth'
-export SAVE_PATH='/home/data/compression/aqlm_cache/llama7/'
-#export WANDB_PROJECT=MY_AQ_EXPS
-#export WANDB_NAME=COOL_EXP_NAME
+# export CUDA_VISIBLE_DEVICES=0   # or e.g. 0,1,2,3
+export MODEL_PATH='/home/cache/models--meta-llama--Llama-2-7b-hf/snapshots/01c7f73d771dfac7d292323805ebc428287df4f9'
+export DATASET_PATH='pajama' #'/home/data/compression/aqlm_cache/red_pajama_llama.pth'
+export SAVE_PATH='/home/cache/aqlm_llama7_repoconf/'
+export WANDB_PROJECT="AQLM"
+export WANDB_NAME="TEST_AQLM"
 
 python main.py $MODEL_PATH $DATASET_PATH \
- --nsamples=1024 \
+ --nsamples=4096 \
  --val_size=128 \
  --num_codebooks=1 \
- --nbits_per_codebook=16 \
+ --nbits_per_codebook=15 \
  --in_group_size=8 \
  --relative_mse_tolerance=0.01 \
+ --finetune_lr=1e-4 \
+ --finetune_adam_beta1=0.90 \
+ --finetune_adam_beta2=0.95 \
  --finetune_batch_size=32 \
- --finetune_max_epochs=10 \
- --finetune_early_stop=3 \
+# --finetune_relative_mse_tolerance=0.001 \
+ --local_batch_size=4 \
+ --finetune_max_epochs=50 \
+# --finetune_early_stop=3 \
  --finetune_keep_best \
- --local_batch_size=1 \
+# --local_batch_size=1 \
  --offload_activations \
- --save $SAVE_PATH
-# --wandb \
+ --save $SAVE_PATH \
+ --wandb 
 # --resume \
 
